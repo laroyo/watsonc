073cdf47965e4146b91a222d77c55a67f4f56f15
Hui Lin <alice8linhui@gmail.com>
2013-03-14 15:20:07 +0100

    Add the new sql file

--- GUI/stift7/hui/watsoncs.sql
+++ GUI/stift7/hui/watsoncs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 04, 2013 at 10:00 PM
+-- Generation Time: Mar 14, 2013 at 03:14 PM
 -- Server version: 5.5.20
 -- PHP Version: 5.3.9
 
@@ -23,12 +23,60 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cfinput`
+--
+
+CREATE TABLE IF NOT EXISTS `cfinput` (
+  `auto_id` int(255) NOT NULL,
+  `job_id` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `job_title` longtext CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `created_date` datetime NOT NULL,
+  `file_name` longtext CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `type_of_units` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `template` varchar(255) NOT NULL,
+  `max_judgements_per_worker` int(11) NOT NULL,
+  `max_judgements_per_ip` int(11) NOT NULL,
+  `units_per_assignment` int(11) NOT NULL,
+  `assignments_per_job` int(11) NOT NULL,
+  `units_per_job` int(11) NOT NULL,
+  `judgements_per_unit` int(11) NOT NULL,
+  `judgements_per_assignment` int(11) NOT NULL,
+  `judgements_per_job` int(255) NOT NULL,
+  `payment_per_unit` decimal(10,2) NOT NULL,
+  `payment_per_assignment` decimal(10,2) NOT NULL,
+  `payment_per_job` decimal(10,2) NOT NULL,
+  `total_payment_per_unit` decimal(10,2) NOT NULL,
+  `total_payment_per_assignment` decimal(10,2) NOT NULL,
+  `total_payment_per_job` decimal(10,2) NOT NULL,
+  `job_comments` longtext CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `job_judgements_made` int(255) NOT NULL,
+  `job_completion` double(10,2) NOT NULL,
+  `run_time` double(10,2) NOT NULL,
+  `status` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  PRIMARY KEY (`auto_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cfinput`
+--
+
+INSERT INTO `cfinput` (`auto_id`, `job_id`, `job_title`, `created_date`, `file_name`, `type_of_units`, `template`, `max_judgements_per_worker`, `max_judgements_per_ip`, `units_per_assignment`, `assignments_per_job`, `units_per_job`, `judgements_per_unit`, `judgements_per_assignment`, `judgements_per_job`, `payment_per_unit`, `payment_per_assignment`, `payment_per_job`, `total_payment_per_unit`, `total_payment_per_assignment`, `total_payment_per_job`, `job_comments`, `job_judgements_made`, `job_completion`, `run_time`, `status`) VALUES
+(1, '176454', 'Find the relation between the two terms', '2013-03-12 21:59:54', 'Chang-location.xlsx', 'NA', 'With definitions and with extra questions', 5, 5, 2, 203, 102, 6, 12, 1218, 1.00, 2.00, 203.00, 6.00, 12.00, 1218.00, 'Run for one week', 0, 0.00, 0.00, 'Running'),
+(2, '176479', 'Find term relations', '2013-03-12 23:58:38', 'Chang-treat.xlsx', 'NA', 'With definitions but without extra questions', 6, 6, 1, 184, 184, 5, 5, 920, 2.00, 2.00, 368.00, 10.00, 10.00, 1840.00, '', 0, 0.00, 0.00, 'Running'),
+(3, '176655', 'Defind relations between two highlighted keywords', '2013-03-13 09:56:52', '70-Sentences-ChangSet5.xlsx', 'NA', 'Without definitions but with extra questions', 8, 8, 1, 109, 109, 5, 5, 545, 5.00, 5.00, 545.00, 25.00, 25.00, 2725.00, '', 0, 0.00, 0.00, 'Running'),
+(4, '176657', 'Defind relations', '2013-03-13 10:10:15', '70-Sentences-ChangSet6.xlsx', 'NA', 'Without definitions and without extra questions', 10, 10, 1, 103, 103, 8, 8, 824, 2.00, 2.00, 206.00, 16.00, 16.00, 1648.00, '', 0, 0.00, 0.00, 'Running'),
+(5, '176680', 'Defind relations', '2013-03-13 14:56:06', '50-1-sentences.csv', 'NA', 'Without definitions but with extra questions', 2, 2, 1, 1599, 1599, 5, 5, 7995, 1.00, 1.00, 1599.00, 5.00, 5.00, 7995.00, '', 0, 0.00, 0.00, 'Running'),
+(6, '176681', 'Find the relation between the two terms', '2013-03-13 15:01:54', 'w21-cause-0.1.txt.filterbykb.sample.csv', 'NA', 'With definitions and with extra questions', 5, 5, 1, 200, 200, 5, 5, 1000, 20.00, 20.00, 4000.00, 100.00, 100.00, 20000.00, '', 0, 0.00, 0.00, 'Running');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `csvinput`
 --
 
 CREATE TABLE IF NOT EXISTS `csvinput` (
   `auto_id` int(255) NOT NULL,
-  `file_id` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `job_id` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `created_date` datetime NOT NULL,
   `file_name` varchar(255) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   `job_title` longtext CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
@@ -37,8 +85,27 @@ CREATE TABLE IF NOT EXISTS `csvinput` (
   `units_per_assignment` int(11) NOT NULL,
   `max_judgement_per_ip` int(11) NOT NULL,
   `cents_per_assignment` decimal(11,0) NOT NULL,
+  `comments` longtext NOT NULL,
   PRIMARY KEY (`auto_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `csvinput`
+--
+
+INSERT INTO `csvinput` (`auto_id`, `job_id`, `created_date`, `file_name`, `job_title`, `judgement_per_unit`, `max_judgement_per_worker`, `units_per_assignment`, `max_judgement_per_ip`, `cents_per_assignment`, `comments`) VALUES
+(1, '3292879846', '2013-03-05 00:23:45', 'job-sentences.csv', 'Find the relation between the two terms', 15, 10, 1, 10, 10, ''),
+(2, '3299829653', '2013-03-05 00:33:09', 'job-sentences.csv', 'Find the relation between the two terms', 15, 10, 1, 10, 10, ''),
+(3, '3952870717', '2013-03-05 15:51:17', 'job-sentences.csv', 'Find relations', 1, 2, 3, 4, 5, ''),
+(4, '1893416898', '2013-03-06 10:35:35', 'w21-contra-0.1.txt.filterbykb.sample-all.csv', 'Defind relations between two highlighted keywords', 20, 5, 1, 10, 15, 'This job will run for one week '),
+(5, '2541316868', '2013-03-06 10:48:51', 'Chang-cause.csv', 'Defind relations between two highlighted keywords', 5, 2, 1, 4, 20, 'It will be expired after one week'),
+(6, '9532497953', '2013-03-06 11:00:30', 'Chang-prevent.csv', 'How to relate the two keywords', 8, 8, 1, 8, 18, 'Run for three days'),
+(7, '1609608770', '2013-03-06 13:48:08', 'Chang-cause.csv', 'test', 15, 10, 1, 10, 1, ''),
+(8, '1894673645', '2013-03-06 16:40:27', 'Chang-cause.csv', 'Defind relations', 10, 10, 1, 10, 5, ''),
+(9, '3759651405', '2013-03-06 17:31:31', 'w21-diagnose-0.1.txt.filterbykb.sample.csv', 'Find the relation between two terms', 10, 5, 1, 5, 0, ''),
+(10, '4420482127', '2013-03-08 14:07:31', '', '', 0, 0, 0, 0, 0, ''),
+(11, '1258179336', '2013-03-11 10:19:00', 'w21-contra-0.1.txt.filterbykb.sample.csv', 'fine', 0, 0, 0, 0, 0, ''),
+(12, '3641101180', '2013-03-11 10:20:29', 'w21-contra-0.1.txt.filterbykb.sample.csv', 'find term relations', 6, 6, 1, 6, 5, 'run for two days');
 
 -- --------------------------------------------------------
 
