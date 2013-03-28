51825e7c901235dc8f32d1bb7f240b7d02a66ac1
Guillermo S <guillelmo@gmail.com>
2013-03-28 10:44:13 +0100

    In general: modified paths and routes to adapt to IBM server. - dataproc.inc: Modified call to workerMetrics script, passing paths for the server - genAnalysis Files.php: assign default job_id (mock, util we get to pass the actual job id)

--- dataproc/genAnalysisFiles.php
+++ dataproc/genAnalysisFiles.php
@@ -6,9 +6,10 @@ if(isset($_GET['job_id'])){
   $job_id = $_GET['job_id'];  
   print("processing data for job: $job_id\n"); 
 } else {
-  print "Please, specify the Job ID of the job that should be processed on the URL<br>"; 
-  print "Example: http://eculture2.cs.vu.nl/dataproc/genAnalysisFiles.php?job_id=178569"; 
-  return; 
+  /* print "Please, specify the Job ID of the job that should be processed on the URL<br>";  */
+  /* print "Example: http://eculture2.cs.vu.nl/dataproc/genAnalysisFiles.php?job_id=178569";  */
+  /* return;  */
+  $job_id = 178934; 
 }
 
 $dp  = new DataProc($job_id); 
@@ -19,9 +20,10 @@ print("Workers labelled as spam: <br>");
 
 $res = $dp->getSpamLabels();
 foreach($res as $spammer_id){
-  print("$spammer_id<br>"); 
+  print("$spammer_id<br><br>"); 
 }
 
-echo "<a href='http://eculture2.cs.vu.nl/data/index.php'>See generated data files</a>"
 
+   echo "<a href='http://129.35.251.201/wcs/data'>See generated data files</a>"
+   		
 ?>
