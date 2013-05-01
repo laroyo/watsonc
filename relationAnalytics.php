43014f4b95b38e245bf6d185d85d272a2f044f09
Guillermo S <guillelmo@gmail.com>
2013-05-01 09:53:07 +0200

    Analytics pages

---
+++ relationAnalytics.php
@@ -0,0 +1,25 @@
+<?php
+if(isset($_GET['relation_id'])){
+  $relation_id = $_GET['relation_id']; 
+  //$job_ids = getJobsInSet($set_id);  
+}
+$relations = array('D'=> "RelName",'S'=> "Symptom",'C'=> "RelName",'M'=> "RelName",'L'=> "RelName",'AW'=> "RelName",'P'=> "RelName",'SE'=> "RelName",'IA'=> "RelName",'PO'=> "RelName",'T' => 'Treats','CI'=> "RelName", 'OTH'=> "RelName", "NONE");
+?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<body>
+<h2>Relation Analytics for "<?= $relations[$relation_id] ?>"</h2>
+
+<h3> Sentences labelled with relation <?= $relations[$relation_id] ?></h3>
+<table>
+<tr><td>Sentence ID</td><td>Agr</td><td> Num Labels</td><td>2nd Relation</td></tr>
+<tr><td><a ref="sentenceAnalytics.php?sentence_id=265614956">265614956</a></td><td>0.0</td><td>0</td><td><a href="relationAnalytics.php?relation=S">Symptom</a></td></tr>
+</table>
+
+<h3>Relation Metrics</h3>
+<p><span title="is a pairwise conditional probability that if relation $R_{i}$ is annotated in a sentence, relation $R_{j}$ is as well.  Information about relation similarity is used in training and evaluation, as it roughly indicates how confusable the linguistic expression of two relations are.  This would indicate, for example, that relation co-learning \cite{Mitchell paper on co-learning} would not work for similar relations."><b>Relation similarity</b></span></p> 
+
+<p><span title="is defined for each relation as the max relation similarity for the relation.  If a relation is very clear, then it will have a low score.  Since techniques like relation co-learning have proven effective, it may be a useful property of a set of relations to exclude ambiguous relations from the set."><b>Relation ambiguity</b></span> </p>
+
+<p><span title="is defined for each relation as the max sentence-relation score for the relation over all sentences.  If a relation has a high clarity score, it means that it is at least possible to express the relation clearly.  We find in our experiments that a lot of relations that exist in structured sources a very difficult to express clearly in language, and are not frequently present in textual sources.  Unclear relations may indicate unattainable learning tasks."><b>Relation clarity</b> </p>
+
---
+++ sentenceAnalytics.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_GET['sentence_id'])){
+  $sentence_id = $_GET['sentence_id']; 
+  //$job_ids = getJobsInSet($set_id);  
+}
+?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<body>
+<h2>Sentence Analytics for sentence <?= $sentence_id ?></h2>
+<table>
+<tr><td>Job ID</td><td>Agr</td><td> Num Labels</td><td>Relation</td></tr>
+<tr><td><a href="job_analytics.php?job_id=179229">179229</td><td>0.0</td><td>0</td><td><a href="relationAnalytics.php?relation_id=T">Treats</a></td></tr>
+</table>
+
+<p><span alt=" is the core crowd truth metric for relation extraction.  It is measured for each relation on each sentence as the cosine of the unit vector for the relation with the sentence vector.  The relation score is used for training and evaluation of the relation extraction system, it is viewed as the probability that the sentence expresses the relation.  This is a fundamental shift from the traditional approach, in which sentences are simply labelled as expressing, or not, the relation, and presents new challenges for the evaluation metric and especially for training"><b>Sentence-relation score</b></span> 0.0</p>
+<p><span alt="is defined for each sentence as the max relation score for that sentence. If all the workers selected the same relation for a sentence, the max relation score will be 1, indicating a clear sentence.   In Figure \ref{fig:crowd-annotations-final}, sentence 735 has a clarity score of 1, whereas sentence 736 has a clarity score of 0.61, indicating a confusing or ambiguous sentence. Sentence clarity is used to weight sentences in training and evaluation of the relation extraction system, since annotators have a hard time classifying them, the machine should not be penalized as much for getting it wrong in evaluation, nor should it treat such training examples as exemplars."><b>Sentence clarity</b> 0.0</span></p>
+
