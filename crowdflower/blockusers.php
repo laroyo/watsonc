f32b0a7377d3986ba3e858d16ffa186f2cad3db8
Oana Inel <oana.inel@gmail.com>
2013-05-29 21:23:52 +0200

    blocking workers script added

--- crowdflower/blockusers.php
+++ crowdflower/blockusers.php
@@ -1,10 +1,6 @@
-<html>
-<head>
-</head>
-<body>
-
 <?php
-
+include_once '../includes/dbinfo.php';
+include_once '../includes/functions.php';
 /* useful functions for printing the results from the web server */
 function objectToArray($obj) {
 	if (is_object($obj)) {
@@ -27,12 +23,12 @@ function arrayToObject($d) {
 	}
 }
 
-function blockSpamWorkers($job_id, $spam_workers) {
+function blockSpamWorkers($job_id, $reason, $spam_workers) {
 	$content_type = "application/json";
-	$api_key = "c6b735ba497e64428c6c61b488759583298c2cf3";
-	$reason = "Your answers were not correct!";
+	$api_key = "b5e3b32b4d29d45c16dc09274e099f731237e35f";
+//	$api_key = "c6b735ba497e64428c6c61b488759583298c2cf3";
 	$data = array('flag' => $reason, 'persist' => 'true'); 
-
+	$responseString = "";
 	foreach($spam_workers as $worker_id) {
 		$url = "https://api.crowdflower.com/v1/jobs/$job_id/workers/$worker_id/flag?key=$api_key";
 		$ch = curl_init();
@@ -43,18 +39,32 @@ function blockSpamWorkers($job_id, $spam_workers) {
 
 		$response = json_decode(curl_exec($ch));
 		$info = curl_getinfo($ch);
-		print_r($info);
 		$array = objectToArray($response);
-		print_r($array);	
-		print_r($response);
+		$keys = array_keys($array);
+		if ($keys[0] == "error") {
+			$responseString .= "An error occurred when blocking contributor $worker_id! ";
+		}
+		else if ($keys[0] == "warning") {
+			$responseString .= "Contributor $worker_id has already been flagged! ";
+		}
+		else if ($keys[0] == "success") { 
+			$responseString .= "Contributor $worker_id flagged! ";  
+			
+			// update blocked_workers table
+			$insertSQL = "INSERT INTO blocked_workers (worker_id, job_id, reason, created_by) 
+				      VALUES ( '$worker_id', '$job_id', '$reason', '{$_SERVER["REMOTE_USER"]}')";
+			if (!mysql_query($insertSQL, $con))
+			{
+				die('Error: ' . mysql_error());
+			}
+		}
 	}
+	return $responseString;
 }
 
-$job_id = $_POST["job_id"];
-$spam_workers = $_POST["spam_workers"]; 
-blockSpamWorkers($job_id, $spam_workers);
+$job_id = $_POST["jobId"];
+$reason = $_POST["reason"];
+$spam_workers = $_POST["workerId"];
+$scriptResponse = blockSpamWorkers($job_id, $reason, $spam_workers);
+echo $scriptResponse;
 ?>
-
-
-</body>
-</html>
