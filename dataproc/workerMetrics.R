9c6741e3e20b595f80848ae428d931752fae46b2
Guillermo S <guillelmo@gmail.com>
2013-06-07 10:34:56 +0200

    Added workerSentenceScore. Modified singletons calculations to adapt it to new function structures (returning a dataframe with id's, instead of a vector of values). Disabled insertFiltWorkers, it needs to be adapted to new table structure

--- dataproc/workerMetrics.R
+++ dataproc/workerMetrics.R
@@ -29,6 +29,9 @@ if(length(args) > 0){
 file_id <- -1
 
 raw_data <- getJob(job_id)
+if("" %in% unique(raw_data$relation)){
+  raw_data <- raw_data[raw_data$relation!="",]
+}
 
 if(dim(raw_data)[1] == 0){
   cat('JOB_NOT_FOUND')
@@ -65,7 +68,8 @@ if(dim(raw_data)[1] == 0){
   numSent <- numSentences(raw_data)
 
   #Singletons: workers with less than 3 judgments (not sufficient to classify them as spammers). 
-  singletons <- belowFactor(data.frame(row.names = worker_ids, numSents=numSent), 'numSents',3)
+  #singletons <- belowFactor(data.frame(row.names = worker_ids, numSents=numSent), 'numSents',3)
+  singletons <- belowFactor(numSent,'numSent',3)
 
   #Remove singletons
   raw_data <- raw_data[!(raw_data$worker_id %in% singletons),]
@@ -95,10 +99,24 @@ if(dim(raw_data)[1] == 0){
     #df <- data.frame(row.names=filtWorkers,numSents=numSent, cos=cosValues, agr=agrValues, annotSentence=(numAnnot/numSent))
     df <- cbind(numSent,cosValues, agrValues,annotSentence)
 
+    if(f == 'NULL'){
+      saveWorkerMetrics(df,job_id)
+
+	  sentRelDf <- sentRelScoreMeasure(raw_data)
+	  sClarity <- sentenceClarity(sentRelDf)
+	  rClarity <- relationClarity(sentRelDf)  
+  
+	  workerSentCos <- workerSentenceCosTable(raw_data)
+	  workerSentScore <- workerSentenceScoreTable(raw_data, workerSentCos, sClarity)            
+	  workerRelScore <- workerRelationScore(raw_data, rClarity, workerSentCos)
+
+	  insertWorkerSentenceScore(workerSentScore, workerSentCos)
+    }
+    
     # Add empty values for filtered out workers
     # missingworkers <- setdiff(worker_ids,filtWorkers)
     # emptyCol <-  rep(0,length(missingworkers))
-  
+
     ## filtrows <- data.frame(row.names=missingworkers,numSents=emptyCol,cos=emptyCol,agr=emptyCol,annotSentence=emptyCol)
     ## df <- rbind(df, filtrows)
     ## df <- df[order(as.numeric(row.names(df))),]
@@ -142,8 +160,9 @@ if(dim(raw_data)[1] == 0){
   sf <- as.data.frame(rowSums(spamFilterOutput > 0) > 1)
   colnames(sf) = 'label'
   spamLabels <- rownames(sf[sf$label==TRUE,,drop=FALSE])
-  
-  insertFiltWorkers(job_id,file_id, '[cos,annotSentence,agr]',spamLabels)
+
+  #FIXME: modify insertFiltWorkers for new table format. 
+  #insertFiltWorkers(job_id,file_id, '[cos,annotSentence,agr]',spamLabels)
 
   numFilteredSentences <- length(unlist(discarded))
   numFilteredWorkers <- length(spamLabels)
@@ -154,7 +173,7 @@ if(dim(raw_data)[1] == 0){
   path <- getFilePath(job_id, folderTypes[['analysisFiles']])
   
   wb.new <- loadWorkbook(paste(path,fname,sep='/'), create = TRUE)
-  
+
   ## createSheet(wb.new, name = "pivot-worker")
   ## writeOutputHeaders(wb.new,"pivot-worker")
 
@@ -164,6 +183,14 @@ if(dim(raw_data)[1] == 0){
   
   writeOutputHeaders(wb.new,"singleton-workers-removed")
   writeWorksheet(wb.new,data=out[rownames(out) %in% setdiff(rownames(out),singletons),],sheet="singleton-workers-removed",startRow=2,startCol=1,header=TRUE,rownames='Worker ID')
+
+  ## createSheet(wb.new, name="workerRelationScore")
+  
+  ## wrs <- workerRelScore
+  ## wrs[is.na(workerRelScore)] <- 0
+  ## wrs$worker_id <- rownames(wrs)
+  
+  ## writeWorksheet(wb.new,data=wrs[,all],sheet="workerRelationScore",startRow=1,startCol=1,header=TRUE,rownames='Worker ID')    
 
   createSheet(wb.new, name = "filtered-out-sentences")
   writeFilteredOutHeaders(wb.new,"filtered-out-sentences")
