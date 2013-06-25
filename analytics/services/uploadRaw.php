a783d8f6a20f9641bbc2ad7f77cb047a5d914eef
Hui Lin <alice8linhui@gmail.com>
2013-06-25 22:15:11 +0200

    Add services folder to analytics for later use

---
+++ analytics/services/uploadRaw.php
@@ -0,0 +1,13 @@
+<?php 
+include_once '../includes/dbinfo.php';
+include_once '../includes/functions.php';
+$fileid = storeFile('rawuploadedfile');
+$lines = getLines($fileid) ;
+$filetitle =  $_POST['title'];
+$filefreeComment =  $_POST['freeComment'];
+
+$query="INSERT INTO `raw_file`( `seedrelationname`, `fileid`, `lines`, `comment`, `createdby`) 
+VALUES ('".$filetitle."',".$fileid.",".$lines.",'".$filefreeComment."','".$_SERVER['REMOTE_USER']."')";
+mysql_query($query) or dieError("uploadRaw 2:<br/>".$query."<br/>".mysql_error());
+header('Location: ../index.php'); //back home
+?>
