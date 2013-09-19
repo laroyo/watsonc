3ea0fba5e2887a14f623d833203e6a22aa720e6e
Oana Inel <oana.inel@gmail.com>
2013-09-19 18:15:36 +0200

    update api key

--- crowdflower/indexcrowdflower.php
+++ crowdflower/indexcrowdflower.php
@@ -2,8 +2,8 @@
 include_once '../includes/dbinfo.php';
 include_once '../includes/functions.php';
 $content_type = "application/json";
-$api_key = "c6b735ba497e64428c6c61b488759583298c2cf3";
-//$api_key = "b5e3b32b4d29d45c16dc09274e099f731237e35f";
+//$api_key = "c6b735ba497e64428c6c61b488759583298c2cf3";
+$api_key = "b5e3b32b4d29d45c16dc09274e099f731237e35f";
 
 $url = "http://api.crowdflower.com/v1/jobs.json?key=".$api_key;
 $uploadDirectory = "Files/";
